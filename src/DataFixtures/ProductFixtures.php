7212057b10b410f4214458e41b0072bda4d37450
massilia saifi <saifi.massilia@gmail.com>
2020-07-03 11:17:41 +0200

    AJOUTE  une entité  de categorie de produits

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -2,11 +2,13 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
-class ProductFixtures extends Fixture
+class ProductFixtures extends Fixture implements DependentFixtureInterface
 {
 
     public function load(ObjectManager $manager)
@@ -23,8 +25,31 @@ class ProductFixtures extends Fixture
                 ->setPrice($faker->numberBetween(1000,35000))
                 ->setCreatedAt($faker->dateTimeBetween('-6 month'))
                 ;
+
+
+            //Recuperation aléatoire d'une catégorie par une réference (on la fait apres avoir creer le category fixtures
+            $categoryReference ='category_' .$faker->numberBetween(0,2);
+            $category =$this->getReference($categoryReference);
+            /** @var Category $category *///il est pas obligatoir (03/07/2020 10h20)7min
+            //$category c'est un objet de la class category
+
+            $product->setCategory($category);
+
+
             $manager->persist($product);
         }
         $manager->flush();
     }
+    /**
+     * Liste des classes de fixtures qui doivent etre chargées avant celle-ci
+
+     */
+    public function getDependencies()
+    {
+        // TODO: Implement getDependencies() method.
+        return [
+            CategoryFixtures::class
+        ];
+    }
 }
+
